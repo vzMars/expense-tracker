ced9bf86b63396a5bdd9e6532924bb5269d17f06
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-06-07 10:42:50 -0400

    added all the routes for the app and wrapping private routes using the RequireAuth and RequireGuest components, there is also an 404 pages for routes that dont exist

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Components
+import RequireAuth from './components/RequireAuth';
+import RequireGuest from './components/RequireGuest';
+
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import NotFound from './pages/NotFound';
+
+// Layouts
+import Layout from './layouts/Layout';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route element={<RequireGuest />}>
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Signup />} />
+          </Route>
+
+          <Route element={<RequireAuth />}>
+            <Route index element={<Home />} />
+          </Route>
+
+          <Route path='404' element={<NotFound />} />
+          <Route path='*' element={<Navigate to='404' replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
