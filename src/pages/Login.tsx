d0e34703887b58ee333eed3699a07294b1d5a4be
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-06-08 09:48:30 -0400

    login and signup pages styling

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,49 @@
+import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
+import Error from '../components/Error';
+
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, error, isLoading } = useLogin();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await login(username, password);
+  };
+
+  return (
+    <main className='lg:max-w-xl lg:flex-1 lg:p-24'>
+      <form
+        className='flex flex-col p-4 lg:rounded-md lg:border-2 lg:border-main-border lg:py-12'
+        onSubmit={handleSubmit}
+      >
+        <h1 className='mb-4 font-lexbold text-4xl lg:mb-6'>Login</h1>
+        <label className='mb-1 font-lexreg'>Username</label>
+        <input
+          type='text'
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+          className='mb-4 rounded-md border-2 border-main-border p-2 lg:mb-6'
+        />
+        <label className='mb-1 font-lexreg'>Password</label>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          className='mb-4 rounded-md border-2 border-main-border p-2 lg:mb-6'
+        />
+        <button
+          disabled={isLoading}
+          className='mb-4 w-full rounded-md bg-main-green px-2.5 py-2 font-lexbold hover:opacity-90 lg:mb-6'
+        >
+          Login
+        </button>
+        {error && <Error errorMessage={error} />}
+      </form>
+    </main>
+  );
 };
 
 export default Login;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,5 +1,57 @@
+import { useState } from 'react';
+import { useSignup } from '../hooks/useSignup';
+import Error from '../components/Error';
+
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const { signup, error, isLoading } = useSignup();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await signup(email, username, password);
+  };
+
+  return (
+    <main className='lg:max-w-xl lg:flex-1 lg:p-24'>
+      <form
+        className='flex flex-col p-4 lg:rounded-md lg:border-2 lg:border-main-border lg:py-12'
+        onSubmit={handleSubmit}
+      >
+        <h1 className='mb-4 font-lexbold text-4xl lg:mb-6'>Sign Up</h1>
+        <label className='mb-1 font-lexreg'>Email</label>
+        <input
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          className='mb-4 rounded-md border-2 border-main-border p-2 lg:mb-6'
+        />
+        <label className='mb-1 font-lexreg'>Username</label>
+        <input
+          type='text'
+          onChange={(e) => setUserName(e.target.value)}
+          value={username}
+          className='mb-4 rounded-md border-2 border-main-border p-2 lg:mb-6'
+        />
+        <label className='mb-1 font-lexreg'>Password</label>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          className='mb-4 rounded-md border-2 border-main-border p-2 lg:mb-6'
+        />
+        <button
+          disabled={isLoading}
+          className='mb-4 w-full rounded-md bg-main-green px-2.5 py-2 font-lexbold hover:opacity-90 lg:mb-6'
+        >
+          Sign Up
+        </button>
+        {error && <Error errorMessage={error} />}
+      </form>
+    </main>
+  );
 };
 
 export default Signup;
