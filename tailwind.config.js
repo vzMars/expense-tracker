6186f76e14e2cd8df8a22491a1bfb4cb2030dcd4
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-06-08 09:47:14 -0400

    added custom colors for errors and the main look of the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        error: {
+          100: '#fd8080',
+          200: '#5c0000',
+          300: '#290000',
+        },
+        main: {
+          border: '#d8dce3',
+          green: '#32d583',
+          greenHover: '#f6fef9',
+        },
+      },
       fontFamily: {
         kgcs: ['kgcs'],
         lexreg: ['lexreg'],
