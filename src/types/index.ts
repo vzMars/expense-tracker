099d504ba8db0a97eb54c6fd2700dd324b705843
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-06-09 10:13:07 -0400

    TransactionContext related types added

--- src/types/index.ts
+++ src/types/index.ts
@@ -23,3 +23,33 @@ export type AuthContextType = {
 export type AuthReducerAction =
   | { type: 'LOGIN'; payload: UserType }
   | { type: 'LOGOUT' };
+
+// TransactionContext
+export type TransactionType = {
+  id: number;
+  title: string;
+  amount: number;
+  transactionDate: Date;
+  description: string;
+  userId: number;
+  categoryId: number;
+  categoryName: string;
+  categoryType: string;
+};
+
+export type TransactionStateType = {
+  transactions: TransactionType[];
+  isLoading: boolean;
+};
+
+export type TransactionContextType = {
+  transactions: TransactionType[];
+  isLoading: boolean;
+  dispatch: React.Dispatch<TransactionReducerAction>;
+};
+
+export type TransactionReducerAction =
+  | { type: 'SET'; payload: TransactionType[] }
+  | { type: 'CREATE'; payload: TransactionType }
+  | { type: 'UPDATE'; payload: TransactionType }
+  | { type: 'DELETE'; payload: number };
