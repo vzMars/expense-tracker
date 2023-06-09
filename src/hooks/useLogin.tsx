6aae2b3f40112c187a3d75dcab250eeaea1f8060
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-06-09 10:16:00 -0400

    when a user logs in also retrieve their transactions

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import { useAuthContext } from './useAuthContext';
+import { useTransactionContext } from './useTransactionContext';
 import { userLogin } from '../services/AuthService';
+import { getTransactions } from '../services/TransactionService';
 
 export const useLogin = () => {
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const { dispatch } = useAuthContext();
+  const { dispatch: transactionDispatch } = useTransactionContext();
 
   const login = async (username: string, password: string) => {
     setIsLoading(true);
@@ -21,7 +24,9 @@ export const useLogin = () => {
     }
 
     if (response.ok) {
+      const transactions = await getTransactions();
       dispatch({ type: 'LOGIN', payload: json.user });
+      transactionDispatch({ type: 'SET', payload: transactions });
       setIsLoading(false);
     }
   };
