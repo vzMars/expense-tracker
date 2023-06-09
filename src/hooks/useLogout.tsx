352c99ba15675ae0bb1389b0827ae59df1bf433a
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-06-09 10:17:58 -0400

    when a user logs out clear the transactions from the context

--- src/hooks/useLogout.tsx
+++ src/hooks/useLogout.tsx
@@ -1,14 +1,17 @@
 import { useAuthContext } from './useAuthContext';
+import { useTransactionContext } from './useTransactionContext';
 import { userLogout } from '../services/AuthService';
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const { dispatch: transactionDispatch } = useTransactionContext();
 
   const logout = async () => {
     const response = await userLogout();
 
     if (response.ok) {
       dispatch({ type: 'LOGOUT' });
+      transactionDispatch({ type: 'SET', payload: [] });
     }
   };
 
